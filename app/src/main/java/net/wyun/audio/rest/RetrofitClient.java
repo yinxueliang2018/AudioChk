3a0734ec8461ba407350ade7ae4ae236beb0c59e
michaelyin <gt8442b@yahoo.com>
2018-04-17 16:32:46 -0400

    rest client.

---
+++ app/src/main/java/net/wyun/audio/rest/RetrofitClient.java
@@ -0,0 +1,17 @@
+package net.wyun.audio.rest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String baseUrl) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
