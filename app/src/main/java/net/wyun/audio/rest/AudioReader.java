f401b7d0d5b47e90954e6c32b92e841ddf7114e4
michaelyin <gt8442b@yahoo.com>
2018-04-17 16:37:12 -0400

    reader for audio files. it send audio file to rest server for reading.

--- app/src/main/java/net/wyun/audio/rest/AudioReader.java
+++ app/src/main/java/net/wyun/audio/rest/AudioReader.java
@@ -4,29 +4,15 @@ import net.wyun.audio.domain.Audio;
 import net.wyun.audio.domain.AudioPayload;
 import net.wyun.audio.domain.ReadResponse;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class AudioReader {
 
-    protected static final String ACCEPT = "accept";
-    protected static final String APPLICATION_JSON = "application/json";
-    protected static final String CONTENT_TYPE = "content-type";
-
-    public static final String API_PATH = "api/audio";
-    private static final  String DEFAULT_HOST_PORT = "http://localhost:8080";
-
+    private static final  String DEFAULT_HOST_PORT = "http://localhost:8080/";
     protected String hostUrl;
 
     /**
@@ -34,74 +20,23 @@ public class AudioReader {
      * @param hostUrl - hostUrl for audio reader server
      * */
     public AudioReader(String hostUrl) {
-        this.hostUrl = DEFAULT_HOST_PORT;
+        this.hostUrl = hostUrl;
     }
 
 
-
-    public Map<String, String> readAudio(Audio audio){
-        URI requestUri = createURI();
-        Map<String, String> response = injectAudio(audio, requestUri);
-
-        return response;
+    public Map<String, String> readAudio(Audio audio) throws IOException {
+        AudioPayload payload = new AudioPayload("audio", audio.getEncodedAudio());
+        return readAudio(payload);
     }
 
-    protected static final String URL_DELIM = "/";
+    public Map<String, String> readAudio(AudioPayload payload) throws IOException {
 
-    protected  URI createURI() {
-        StringBuilder builder = new StringBuilder(this.hostUrl);
-        builder.append(URL_DELIM).append(API_PATH);
+        Call<Map<String, String>> call = RetrofitClient.getClient(hostUrl)
+                .create(AudioApiService.class)
+                .readAudio(payload);
 
-        String requestString = builder.toString();
-        URI uri = null;
-
-        try {
-            uri = new URI(requestString);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Unable to validate host URI: " +  requestString);
-        }
-
-        return uri;
-    }
-
-
-
-    protected  Map<String, String> injectAudio(Audio audio, URI request) {
-
-        RestTemplate template = new RestTemplate();
-
-        List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
-
-        converters.add(new MappingJackson2HttpMessageConverter());
-
-        AudioPayload payload = new AudioPayload();
-        payload.setEncodedAudio(audio.getEncodedAudio());
-        payload.setType("audio");
-
-
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.set(CONTENT_TYPE, APPLICATION_JSON);
-        headers.set(ACCEPT, APPLICATION_JSON);
-
-
-        HttpEntity<AudioPayload> entity = new HttpEntity<AudioPayload>(payload, headers);
-
-        template.setMessageConverters(converters);
-
-
-        ResponseEntity<ReadResponse> response = template.postForEntity(request, entity, ReadResponse.class);
-
-        if (HttpStatus.OK != response.getStatusCode()) {
-
-        }
-
-        // hack for marshalling
-        //ResponseContainer container = template.postForObject(request, entity, ResponseContainer.class);
-        Map<String, String> valueMap = response.getBody().getValues();
-        //Map<String, String> valueMap = container.getReadResponse().getValues();
-        return valueMap;
-
+        Response<Map<String, String>> resp = call.execute();
+        return resp.body();
     }
 
 }
